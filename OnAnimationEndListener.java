24a779ddf6cab6de4966fcf24fdde3571bdb9c6a
inkirby <inkirby@gmail.com>
2017-05-29 17:18:00 +0700

    OnAnimationEndListender

---
+++ OnAnimationEndListener.java
@@ -0,0 +1,6 @@
+import android.view.animation.Animation;
+
+public abstract class OnAnimationEndListener implements Animation.AnimationListener {
+	@Override public void onAnimationRepeat(Animation animation) {}
+	@Override public void onAnimationStart(Animation animation) {}
+}
